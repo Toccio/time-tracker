3bcd60d683e4b5e31937f3d43d6ffd6c7710ac10
Toccio <luca.t.visaggio@gmail.com>
2023-04-18 20:19:24 +0200

    some random user and project created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts "Cleaning DB..."
+Project.destroy_all
+puts "Creating DB..."
+
+20.times do
+  project = Project.create (
+    name: Faker::Book.title,
+  )
+  puts "Project with id #{Project.id} created!"
+end
+
+10.times do
+  User.create(
+    email: "scheggia1190@gmail.com",
+    password: "123456",
+    nickname: Faker::Games::Heroes.name,
+  )
+end
+puts "Finished🚀"
