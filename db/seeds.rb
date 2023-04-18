b09c5030ac908dca42bad0a648ac5582bb647962
Toccio <luca.t.visaggio@gmail.com>
2023-04-18 21:47:53 +0200

    project added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 require 'faker'
+require "open-uri"
 
 puts "Cleaning DB..."
 Project.destroy_all
@@ -10,6 +11,12 @@ puts "Creating DB..."
     email: "scheggia1190@gmail.com",
     password: "123456",
     nickname: Faker::Games::Heroes.name,
+  )
+end
+
+10.times do
+  Project.create(
+    name: Faker::Games::Heroes.name
   )
 end
 puts "Finished🚀"
