24438f192d225a134946460b0453767a30b82c0c
Toccio <luca.t.visaggio@gmail.com>
2023-04-18 22:18:59 +0200

    clock time fild added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_18_182156) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_18_195318) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,14 +20,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_18_182156) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "comment"
+    t.string "tempo"
     t.index ["project_id"], name: "index_clocks_on_project_id"
     t.index ["user_id"], name: "index_clocks_on_user_id"
   end
 
   create_table "projects", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "first_name"
   end
 
   create_table "users", force: :cascade do |t|
